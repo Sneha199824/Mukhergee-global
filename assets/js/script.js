40cc3a93ebb8f32266ae8823c9262b8c2f57687d
snehajakhar <116378892+Sneha199824@users.noreply.github.com>
2024-06-28 14:20:48 +0530

    create

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+ function showImage(src) {
+      var mainImg = document.getElementById('main-img-box');
+      mainImg.src = src;
+      document.querySelector('.main-image-box').style.display = 'block';
+    }
